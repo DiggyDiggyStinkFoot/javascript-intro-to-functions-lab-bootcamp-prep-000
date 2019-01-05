c23cdec91590a6f9e3843fd5b5419da3c6c158f2
Wannawuth Damrongchai <wdamrongchai@hotmail.com>
2019-01-05 05:54:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 function shout(string){
   return string.toUpperCase()
 }
+shout('HELLO')
 
 function whisper(string){
   return string.toLowerCase()
 }
+whisper('hello')
 
 function logShout(string){
   return console.log(string.toUpperCase())
 }
-logShout ('hello')
+logShout ('HELLO')
 
 function logWhisper(string){
   return console.log(string.toLowerCase())
